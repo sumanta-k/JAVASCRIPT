4897c1644836187dc158485646f12b9ac9820d67
happy <mohantasumanta601@gmail.com>
2023-08-19 09:37:46 +0530

    added datatypes

---
+++ DOC/index.js
@@ -0,0 +1,22 @@
+const name = 'happy';
+
+const age = 21;
+const result = 55.5;
+const hasGoodMark = false;
+const aptNumber = null;
+let score = undefined;
+const n = 874875348934347837483n;
+
+//reference type
+const numbers = [1,2,3,4];
+const person ={name:'happy',
+age:21,
+occupation:'student'
+    };
+    function Hello(){
+        alert('hello, happy have a nice start');
+    }
+const outPut = Hello;
+
+Hello();
+console.log(outPut, typeof outPut);
